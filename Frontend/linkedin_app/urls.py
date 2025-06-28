7ffe8b7682771c9bc6bddb8fb24ff3cb3d08eee1
JuanDiHS <143758164+JuanDiHS@users.noreply.github.com>
2025-06-27 20:00:07 -0500

    Delete Frontend directory

--- Frontend/linkedin_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name='app_home'),
-    path('login/', views.login, name='app_login'),
-    path('employes', views.employes, name='app_employes')
-
-]
